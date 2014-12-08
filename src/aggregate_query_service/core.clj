ae0eb518a3155fe2b9cb3812343e4423879f2b6d
mihirk <mihir.khatwani@gmail.com>
2014-12-08 15:56:30 +0530

    Mihir | Code to read json, and go through all and fire queries

--- src/aggregate_query_service/core.clj
+++ src/aggregate_query_service/core.clj
@@ -5,3 +5,16 @@
 (defn read-config
   [config-file]
   (json/read-str (slurp config-file) :key-fn keyword))
+
+(defn fire-query
+  [{query :query}]
+  (println query))
+
+(defn fire-queries
+  [{queries :queries}]
+  (map fire-query queries)
+  )
+
+(defn execute-all-queries
+  [config-file]
+  (map fire-queries (read-config config-file)))
--- test/aggregate_query_service/core_test.clj
+++ test/aggregate_query_service/core_test.clj
@@ -1,14 +1,12 @@
 (ns aggregate-query-service.core-test
-  (:import (java.io FileNotFoundException)
-           (java.nio.charset MalformedInputException))
   (:require [clojure.test :refer :all]
             [aggregate-query-service.core :refer :all :as aqs]))
 
 (defn test-config-mapping
-  ( []
-  (aqs/read-config "resources/sample_config.json"))
-  ( [config-file]
-  (aqs/read-config config-file)))
+  ([]
+    (aqs/read-config "resources/sample_config.json"))
+  ([config-file]
+    (aqs/read-config config-file)))
 
 (deftest read-config-file-failure
   (testing "Throw exception on file read failure"
@@ -18,3 +16,7 @@
   (testing "read json config and map it to json"
     (is (= [{:queryGroupname "Query Group 1" :queries [{:queryName "Query 1", :query "select * from something;"} {:queryName "Query 2", :query "select * from something_else;"}]} {:queryGroupname "Query Group 2", :queries [{:queryName "Query 1", :query "select * from one_more_thing;"} {:queryName "Query 2", :query "select * from another_thing;"}]}]
            (test-config-mapping)))))
+
+(deftest execute-all-queries-from-config
+  (testing "Execute all queries"
+    (println (aqs/execute-all-queries "resources/sample_config.json"))))
