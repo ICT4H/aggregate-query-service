2ebfb5d4f1f167c049a3626ffdcf9191ab4e260c
mihirk <mihir.khatwani@gmail.com>
2015-01-14 19:02:07 +0530

    Mihir | Changing to 5.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                            [midje "1.6.3"]
                            [org.xerial/sqlite-jdbc "3.8.7"]
                            [org.bahmni/clojure-test-datasetup "1.0-SNAPSHOT"]
-                           [org.bahmni.module/common "1.0"]
+                           [org.bahmni.module/common "5.2-SNAPSHOT"]
                            [freemarker-clj "0.1.0"]
                            [http.async.client "0.5.2"]
                            [yesql "0.4.0"]]
